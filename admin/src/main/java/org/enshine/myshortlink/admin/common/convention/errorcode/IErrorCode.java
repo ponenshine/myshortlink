f704da673aa8bc5a6a4817b415fd1da5b8f55643
enshy <2469313424@qq.com>
2024-03-14 22:14:43 +0800

    错误码规约

---
+++ admin/src/main/java/org/enshine/myshortlink/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,14 @@
+package org.enshine.myshortlink.admin.common.convention.errorcode;
+
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
