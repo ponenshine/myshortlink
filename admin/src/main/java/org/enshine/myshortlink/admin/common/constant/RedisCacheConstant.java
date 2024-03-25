37c1969413d57e65839cc1c92f3e51645f161317
enshine <2469313424@qq.com>
2024-03-25 18:05:15 +0800

    redis缓存常量类

---
+++ admin/src/main/java/org/enshine/myshortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,5 @@
+package org.enshine.myshortlink.admin.common.constant;
+
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY="shortlink:lock_user-register:";
+}
