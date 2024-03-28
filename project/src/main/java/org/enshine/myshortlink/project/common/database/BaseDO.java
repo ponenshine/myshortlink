e54428a9d088612245ff8a857594208c8d36cbe5
enshine <2469313424@qq.com>
2024-03-28 13:59:24 +0800

    BaseDO日期类格式化

--- project/src/main/java/org/enshine/myshortlink/project/common/database/BaseDO.java
+++ project/src/main/java/org/enshine/myshortlink/project/common/database/BaseDO.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -16,12 +17,14 @@ public class BaseDO {
      * 创建时间
      */
     @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     /**
      * 更新时间
      */
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     /**
