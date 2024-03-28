2060ce7a3fd3abc78a1894108b8c5f1ea228fecc
enshine <2469313424@qq.com>
2024-03-28 13:58:43 +0800

    nacos、openfeign配置

--- admin/src/main/java/org/enshine/myshortlink/admin/AdminApplication.java
+++ admin/src/main/java/org/enshine/myshortlink/admin/AdminApplication.java
@@ -3,9 +3,13 @@ package org.enshine.myshortlink.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("org.enshine.myshortlink.admin.dao.mapper")
+@EnableDiscoveryClient
+@EnableFeignClients("org.enshine.myshortlink.admin.remote")
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class,args);
--- project/src/main/java/org/enshine/myshortlink/project/ProjectApplication.java
+++ project/src/main/java/org/enshine/myshortlink/project/ProjectApplication.java
@@ -3,9 +3,11 @@ package org.enshine.myshortlink.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("org.enshine.myshortlink.project.dao.mapper")
+@EnableDiscoveryClient
 public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class,args);
