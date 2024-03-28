c8fa2791cbc49479c4c43c5604f0c0871e1d268c
enshine <2469313424@qq.com>
2024-03-28 13:54:58 +0800

    UserInfoDTO类链式编程注解

--- admin/src/main/java/org/enshine/myshortlink/admin/common/biz/user/UserInfoDTO.java
+++ admin/src/main/java/org/enshine/myshortlink/admin/common/biz/user/UserInfoDTO.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Accessors(chain = true)
 public class UserInfoDTO {
 
     /**
