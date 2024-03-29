75ea2946a183bce6ed2d36f2af8f51217cde5abd
enshine <2469313424@qq.com>
2024-03-30 00:32:32 +0800

    redis缓存常量类

--- project/src/main/java/org/enshine/myshortlink/project/common/constant/RedisCacheConstant.java
+++ project/src/main/java/org/enshine/myshortlink/project/common/constant/RedisCacheConstant.java
@@ -1,5 +1,9 @@
 package org.enshine.myshortlink.project.common.constant;
 
 public class RedisCacheConstant {
-    public static final String KEY_LINK_CREATE="shortlink:key_link-create:";
+    public static final String KEY_LINK_CREATE = "shortlink:key_link-create:";
+    public static final String KEY_URI = "shortlink:key_uri:";
+    public static final String KEY_NULL_URI = "shortlink:key_null_uri:";
+    public static final String LOCK_URI = "shortlink:lock_uri:";
+    public static final Integer LOCK_WAIT_SECONDS = 5;
 }
