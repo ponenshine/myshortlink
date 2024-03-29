eaee699e614ff0453170ad84b395bd82ab0eb242
enshine <2469313424@qq.com>
2024-03-30 00:31:45 +0800

    字符串常量类

---
+++ project/src/main/java/org/enshine/myshortlink/project/common/constant/StringConstant.java
@@ -0,0 +1,6 @@
+package org.enshine.myshortlink.project.common.constant;
+
+public class StringConstant {
+    public static final String PAGE_NOT_FOUND="page/notfound";
+    public static final String PAGE_ERROR="page/error";
+}
